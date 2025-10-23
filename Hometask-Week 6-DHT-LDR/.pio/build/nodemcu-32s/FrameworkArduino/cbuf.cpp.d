76e7ecf3f9da99611a0c7d9cf5995aacf04ea071
NimraJaved205 <officialnimra205@gmail.com>
2025-10-24 00:50:23 +0500

    Delete Hometask-Week 6-DHT-LDR directory

--- Hometask-Week 6-DHT-LDR/.pio/build/nodemcu-32s/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/nodemcu-32s/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/Nimra/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/Nimra/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
